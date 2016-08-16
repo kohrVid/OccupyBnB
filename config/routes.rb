9bdc3ef4a8b4bff42771f43f2657ec2b31af1c0d
kohrVid <kohrVid@zoho.com>
2016-08-16 21:45:18 +0100

    Added a pages controller and changed the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+  resources :pages, only: [:index] do
+    collection do
+      get 'pages/help'
+      get 'pages/privacy'
+      get 'pages/tos'
+    end
+  end
+
   devise_scope :squatter do
     devise_for :squatters, controllers: { omniauth_callbacks: "callbacks" }
   end
 
-  root to: "abodes#index"
   resources :abodes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
