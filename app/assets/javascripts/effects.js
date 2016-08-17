6fa82c6e9b5fdee2b7e019bc0526b99b95c26e78
kohrVid <kohrVid@zoho.com>
2016-08-17 18:24:35 +0100

    Adding more styles to the pages

---
+++ app/assets/javascripts/effects.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  let hue = parseInt(Math.random() * 360);
+  $(".header-container").css("filter", String(`hue-rotate(${ hue }deg)`));
+  $(".header-container").css("-webkit-filter", String(`hue-rotate(${ hue }deg)`));
+  $(".btn-primary").css("filter", String(`hue-rotate(${ hue }deg)`));
+  $(".btn-primary").css("-webkit-filter", String(`hue-rotate(${ hue }deg)`));
+  $("#main-logo").css("color", String(`hue-rotate(${ hue }deg)`));
+});
