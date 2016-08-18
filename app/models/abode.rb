1eefe020a4ea3b1f76fdd5392bccf3de377023cd
kohrVid <kohrVid@zoho.com>
2016-08-18 22:17:46 +0100

    Added relationship between Squatters, Admins and Abodes

--- app/models/abode.rb
+++ app/models/abode.rb
@@ -1,3 +1,12 @@
 class Abode < ApplicationRecord
+  belongs_to :submitted_by, class_name: "Squatter", 
+	      foreign_key: :submitted_by_id
+  belongs_to :approved_by, class_name: "Admin", foreign_key: :approved_by_id
+
   scope :approved, -> { where(approved: true) }
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :location, presence: true
+  validates :submitted_by, presence: true
+
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,3 @@
 class Admin < Squatter
+  has_many :approved_abodes, class_name: "Abode", foreign_key: :approved_by
 end
