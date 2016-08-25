a5443a16e4caa6995ffdf8f8901cf0c7d3c04245
kohrVid <kohrVid@zoho.com>
2016-08-25 14:27:07 +0100

    Pushing to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   devise_scope :squatter do
-    devise_for :squatter, controllers: { 
+    devise_for :squatters, controllers: { 
       sessions: "squatters/sessions",
       registrations: "squatters/registrations",
       passwords: "squatters/passwords",
