b2a7ccbd0cf0dbaf0b8b212c03df46a2dd44a87f
kohrVid <kohrVid@zoho.com>
2016-08-26 11:43:35 +0100

    Mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,14 +37,14 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { 
+  config.action_mailer.default_options = { 
     host: Rails.application.secrets.mailer_host, 
     from: Rails.application.secrets.mailer_from
   }
+  Rails.application.routes.default_url_options[:host] = Rails.application.secrets.mailer_host
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  Rails.application.routes.default_url_options[:host] = Rails.application.secrets.mailer_host
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
