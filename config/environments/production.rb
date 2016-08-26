d1e1a17637eba0e3d170b9bd2f6818eb4044f82e
kohrVid <kohrVid@zoho.com>
2016-08-26 16:29:48 +0100

    Fixing typo?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = ActionSupport::Logger::Formatter.new
 
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
